24a38f9b139a9babee8e13b9f0971cd70bf108c9
PLKomp\PL <piotr1214@o2.pl>
2017-10-13 21:28:52 +0200

    With annotation.

--- src/com/pl/hellocontroller/HelloController.java
+++ src/com/pl/hellocontroller/HelloController.java
@@ -2,18 +2,38 @@ package com.pl.hellocontroller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloController extends AbstractController{
+// WITHOUT ANNOTATION
+//public class HelloController extends AbstractController{
+//	
+//	@Override
+//	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+//			HttpServletResponse response) throws Exception {
+//		ModelAndView modelandview = new ModelAndView("HelloPage");
+//		modelandview.addObject("welcomeMessage","Hi User, welcome to the first Spring MVC Application");
+//		
+//		return modelandview;
+//	}
+//	
+//REMOVED FROM spring-dispatcher-servlet.xml
+//<bean name="/welcome.html" class="com.pl.hellocontroller.HelloController"/>
+//<bean id="HandlerMapping" class="org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping"/>
 	
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		ModelAndView modelandview = new ModelAndView("HelloPage");
-		modelandview.addObject("welcomeMessage","Hi User, welcome to the first Spring MVC Application");
+	@Controller
+	public class HelloController {
 		
-		return modelandview;
-	}
+		@RequestMapping("/welcome")
+		public ModelAndView helloWorld() {
+			ModelAndView model = new ModelAndView("HelloPage");
+			model.addObject("msg","Hello world, welcome to the first Spring MVC Application");
+			
+			return model;
+		}
+	
 
 }
